5bd2f902476adba8bb3548acebe0b98f1b59889f
Jan Klemkow <j.klemkow@wemelug.de>
2014-09-09 22:12:40 +0200

    fix: wrong use of memset

--- mz.c
+++ mz.c
@@ -177,7 +177,7 @@ MD5Final(u_int8_t digest[16], struct MD5Context *ctx)
 
 	byteSwap(ctx->buf, 4);
 	memcpy(digest, ctx->buf, 16);
-	memset(ctx, 0, sizeof(ctx));    /* In case it's sensitive */
+	memset(ctx, 0, sizeof(*ctx));   /* In case it's sensitive */
 }
 
 /* The four core functions - F1 is optimized somewhat */
